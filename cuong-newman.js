5a73aa230629b0fc312d408ac48a60affe6d7e5e
CuongQCMCVGo <cuongtq@mcvgo.com>
2025-06-01 22:34:07 +0700

    add junit report

--- cuong-newman.js
+++ cuong-newman.js
@@ -1,7 +1,15 @@
 const newman = require("newman");
- 
+
 newman.run({
     collection: require("./AutoGenerateProvinceDistrictWard.postman_collection.json"),
     environment: require('./Goship-CA.postman_environment.json'),
-    reporters: ["cli", "htmlextra"]
+    reporters: ["cli", "htmlextra", "junitfull"],
+    reporter: {
+        junitfull: {
+            export: './newman/results.xml'
+        },
+        htmlextra: {
+            export: './newman/report.html'
+        }
+    }
 });
